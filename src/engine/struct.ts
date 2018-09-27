ee4723a47ab8dac1cfc0d437d34d57278247db4b
KodyJKing <kody.j.king@gmail.com>
2018-09-27 13:44:06 -0700

    Refactored struct.

--- src/engine/struct.ts
+++ src/engine/struct.ts
@@ -1,68 +1,53 @@
-type Properties = [string, number][]
-type Getters = { [name: string]: (struct: number) => number }
-type Setters = { [name: string]: (struct: number, value: number) => number }
+type Properties = [string, number, number][]
 
 const mask = (width: number) => (1 << width) - 1
 
-function createConstructor(properties: Properties) {
-    let names = properties.map( (component) => component[0] )
-    let components: string[] = []
+function propertiesWithShift(properties: [string, number][]) {
     let shift = 0
-    for (let component of properties) {
-        let [name, width] = component
-        components.push( `(${name} << ${shift})` )
+    let result: Properties = []
+    for (let [name, width] of properties) {
+        result.push([name, width, shift])
         shift += width
     }
-    let result = new Function(...names, "return " + components.join(" | "))
+    return result
+}
+
+function fromArguments(properties: Properties) {
+    let names = properties.map( (component) => component[0] )
+    let parts = properties.map( ([name, width, shift]) => `(${name} << ${shift})` )
+    let result = new Function(...names, "return " + parts.join(" | "))
     return result as (...args: number[]) => number
 }
 
-function createToObject(properties: Properties) {
-    let components: string[] = []
-    let shift = 0
-    for (let component of properties) {
-        let [name, width] = component
-        components.push(`${name}: (struct >> ${shift}) & ${mask(width)}`)
-        shift += width
-    }
-    let result = new Function("struct", `return { ${components.join(" , ")} }`)
+function toObject(properties: Properties) {
+    let parts = properties.map( ([name, width, shift]) => `${name}: (struct >> ${shift}) & ${mask(width)}` )
+    let result = new Function("struct", `return { ${parts.join(" , ")} }`)
     return result as (struct: number) => { [name: string]: number }
 }
 
-function createFromObject(properties: Properties) {
-    let components: string[] = []
-    let shift = 0
-    for (let component of properties) {
-        let [name, width] = component
-        components.push(`(object.${name} << ${shift})`)
-        shift += width
-    }
-    let result = new Function("object", "return " + components.join(" | "))
+function fromObject(properties: Properties) {
+    let parts = properties.map( ([name, width, shift]) => `(object.${name} << ${shift})` )
+    let result = new Function("object", "return " + parts.join(" | "))
     return result as (object: { [name: string]: number }) => number
 }
 
-export default function struct(properties: Properties) {
+export default function struct(properties: [string, number][]) {
     let bitCount = properties.map( (component) => component[1] ).reduce( (x, y) => x + y )
     if (bitCount > 32)
         throw new Error("Components don't fit 32 bit number.")
 
-    let get: Getters = {}
-    let set: Setters  = {}
+    let _properties = propertiesWithShift(properties)
 
-    let netShift = 0
-    for (let component of properties) {
-        let [name, width] = component;
+    let get: { [name: string]: (struct: number) => number } = {}
+    let set: { [name: string]: (struct: number, value: number) => number }  = {}
 
-        ((shift: number) => { // Have to use an iffy to freeze the value of shift in the closure.
-            let getMask = mask(width)
-            get[name] = (struct: number) => (struct >> shift) & getMask
+    for (let [name, width, shift] of _properties) {
+        let getMask = mask(width)
+        get[name] = (struct: number) => (struct >> shift) & getMask
 
-            let setMask = ~(getMask << shift) // Used to clear components bits.
-            set[name] = (struct: number, value: number) => (struct & setMask) | (value << shift)
-        })(netShift)
-
-        netShift += width
+        let setMask = ~(getMask << shift) // Used to clear components bits.
+        set[name] = (struct: number, value: number) => (struct & setMask) | (value << shift)
     }
 
-    return { create: createConstructor(properties), toObject: createToObject(properties), fromObject: createFromObject(properties), get, set }
+    return { create: fromArguments(_properties), toObject: toObject(_properties), fromObject: fromObject(_properties), get, set }
 }
