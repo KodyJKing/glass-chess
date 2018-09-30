1d549e15d7b70e6263600eca01c457be652a1159
KodyJKing <kody.j.king@gmail.com>
2018-09-29 21:20:56 -0700

    Added knight move gen, board parser and improved move gen unit tests.

--- src/engine/Game.test.ts
+++ src/engine/Game.test.ts
@@ -5,40 +5,94 @@ import Piece from "./Piece";
 import { Type } from "./Type";
 import { Color } from "./Color";
 
-test("game", t => {
-    let game = new Game()
-    game.toString()
-    // console.log("\n" + game.toString())
+function compareBoards(a: string, b: string) {
+    let format = (x) => (
+        x.split("\n")
+        .filter( (x) => x.length > 0 )
+        .map( (x) => x.trim() )
+    ).join("\n")
+    return format(a) === format(b)
+}
+
+test("parseBoard", t => {
+    let board = `
+        r n b q k b n r
+        p p p p p p p p
+        . . . . . . . .
+        . . . . . . . .
+        . . . . . . . .
+        . . . . . . . .
+        P P P P P P P P
+        R N B Q K B N R`
+    let game = Game.parseBoard(board)
+    t.true(compareBoards(board, game.toString()))
     t.pass()
 })
 
 test("generateMoves_Rook", t => {
-    let game = new Game()
+    let game = new Game().standardSetup()
     let x = 0
     let y = 2
     let pos = Position.create(x, y)
     game.pieces[pos] = Piece.create(Type.Rook, Color.Black, 0)
-    
+
+    let expected = `
+        r n b q k b n r
+        p p p p p p p p
+        r # # # # # # #
+        # . . . . . . .
+        # . . . . . . .
+        # . . . . . . .
+        # P P P P P P P
+        R N B Q K B N R`
+
     // console.log("\n" + game.toString())
-
     let moves = game.generateMovesAt(pos)
-    for (let move of moves.map(Position.toObject))
-        t.true(move.x === x || move.y === y)
-
-    // console.log("\n" + game.toString(moves))
-
-    t.pass()
+    let actual = game.toString(moves)
+    t.true(compareBoards(expected, actual))
+    // console.log("\n" + actual)
 })
 
-test("generateMoves_Rook", t => {
-    let game = new Game()
-    let x = 0
-    let y = 1
-    let pos = Position.create(x, y)
+test("generateMoves_Pawn", t => {
+    let game = new Game().standardSetup()
+    let pos = Position.create(0, 1)
+
+    let expected = `
+        r n b q k b n r
+        p p p p p p p p
+        # . . . . . . .
+        # . . . . . . .
+        . . . . . . . .
+        . . . . . . . .
+        P P P P P P P P
+        R N B Q K B N R`
 
     // console.log("\n" + game.toString())
     let moves = game.generateMovesAt(pos)
-    // console.log("\n" + game.toString(moves))
+    let actual = game.toString(moves)
+    t.true(compareBoards(expected, actual))
+    // console.log("\n" + actual)
+})
 
-    t.pass()
+
+test("generateMoves_Knight", t => {
+    let game = new Game()
+    let pos = Position.create(3, 3)
+    game.pieces[pos] = Piece.create(Type.Knight, Color.Black, 0)
+
+    let expected = `
+         . . . . . . . . 
+         . . # . # . . .
+         . # . . . # . .
+         . . . n . . . .
+         . # . . . # . .
+         . . # . # . . .
+         . . . . . . . .
+         . . . . . . . .`
+
+    // console.log("\n" + game.toString())
+    let moves = game.generateMovesAt(pos)
+    let actual = game.toString(moves)
+    t.true(compareBoards(expected, actual))
+    // console.log("\n" + actual)
 })
--- src/engine/Game.ts
+++ src/engine/Game.ts
@@ -13,8 +13,8 @@ function pieceToChar(piece: number) {
 }
 
 function charToPiece(char: string) {
-    let type = pieceToCharStr.indexOf(char)
-    let color = char === char.toLowerCase() ? Color.Black : Color.White
+    let type = pieceToCharStr.indexOf(char.toLowerCase())
+    let color = (char === char.toLowerCase()) ? Color.Black : Color.White
     return Piece.create(type, color, 0)
 }
 
@@ -22,7 +22,6 @@ export class Game {
     pieces: number[]
     constructor() {
         this.pieces = new Array(64)
-        this.standardSetup()
     }
 
     standardSetup() {
@@ -33,6 +32,26 @@ export class Game {
             this.pieces[Pos(x, 7)] = Piece.create(type, Color.White, 0)
             this.pieces[Pos(x, 6)] = Piece.create(Type.Pawn, Color.White, 0)
         }
+        return this
+    }
+
+    static parseBoard(board: string) {
+        let rows = 
+            board.split("\n")
+            .filter( (row) => row.length > 0 ) // Remove empty lines.
+            .map( 
+                (row) => 
+                    row.replace(/ /g, "")     // Remove whitespace.
+                    .split("")                // Convert to char array.
+                    .map(charToPiece)
+            )
+        let game = new Game()
+        for (let y = 0; y < 8; y++) {
+            for (let x = 0; x < 8; x++) {
+                game.pieces[Pos(x, y)] = rows[y][x]
+            }
+        }
+        return game
     }
 
     toString(moves?: number[]) {
@@ -79,7 +98,7 @@ export class Game {
         this.slide(x, y, 0, 1, max, color, moves)
     }
 
-    slideDiagnols(x: number, y: number, max: number, color: Color, moves: number[]) {
+    slideDiagonals(x: number, y: number, max: number, color: Color, moves: number[]) {
         this.slide(x, y, 1, 1, max, color, moves)
         this.slide(x, y, -1, 1, max, color, moves)
         this.slide(x, y, 1, -1, max, color, moves)
@@ -99,18 +118,35 @@ export class Game {
             case Type.Pawn: {
                 let dy = color == Color.White ? -1: 1
                 this.slide(x, y, 0, dy, moved ? 1 : 2, color, moves)
+
                 let length = moves.length
                 if (this.slide(x, y, -1, dy, 1, color, moves) === undefined)
                     moves.length = length
                 length = moves.length
                 if (this.slide(x, y, 1, dy, 1, color, moves) === undefined)
                     moves.length = length
+
                 break
             }
-            case Type.Bishop: { this.slideDiagnols(x, y, 7, color, moves); break }
+            case Type.Knight: {
+                // QI, +x, +y
+                this.slide(x, y, 2, 1, 1, color, moves)
+                this.slide(x, y, 1, 2, 1, color, moves)
+                // QII, -x, +y
+                this.slide(x, y, -2, 1, 1, color, moves)
+                this.slide(x, y, -1, 2, 1, color, moves)
+                // QIII, -x, -y
+                this.slide(x, y, -2, -1, 1, color, moves)
+                this.slide(x, y, -1, -2, 1, color, moves)
+                // QIV, +x, -y
+                this.slide(x, y, 2, -1, 1, color, moves)
+                this.slide(x, y, 1, -2, 1, color, moves)
+                break
+            }
+            case Type.Bishop: { this.slideDiagonals(x, y, 7, color, moves); break }
             case Type.Rook: { this.slideCardinals(x, y, 7, color, moves); break }
-            case Type.Queen: { this.slideDiagnols(x, y, 7, color, moves); this.slideCardinals(x, y, 7, color, moves); break }
-            case Type.King: { this.slideDiagnols(x, y, 1, color, moves); this.slideCardinals(x, y, 1, color, moves); break }
+            case Type.Queen: { this.slideDiagonals(x, y, 7, color, moves); this.slideCardinals(x, y, 7, color, moves); break }
+            case Type.King: { this.slideDiagonals(x, y, 1, color, moves); this.slideCardinals(x, y, 1, color, moves); break }
         }
         return moves
     }
