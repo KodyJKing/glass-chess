921d30402f8d15e537caf99c66697b0decef7c0d
Kody King <kody.j.king@gmail.com>
2018-11-10 03:18:31 -0800

    Copied glass' setup for svg file support.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,46 @@
-const path = require('path')
+const path = require("path")
 const webpack = require("webpack")
-const webroot = path.join(__dirname, 'www')
+const webroot = path.join(__dirname, "www")
 const BundleAnalyzer = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
 
 const config = {
-    entry: './lib/app/main.js',
+    entry: "./lib/app/main.js",
     output: {
         path: webroot,
-        filename: 'index.js'
+        filename: "[name].js"
     },
-    devtool: 'cheap-source-map',
+    devtool: "cheap-source-map",
     devServer: {
-        contentBase: webroot
+        contentBase: webroot,
+
+        host: "0.0.0.0",
+        port: 8080,
+        proxy: [{
+            path: "/api/*",
+            target: "http://localhost:3000",
+            secure: false
+        }]
+
     },
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    //  ignore the node "crypto" which is required by sjcl
+    plugins: [
+        new webpack.IgnorePlugin(/^crypto$/),
+    ]
 }
 
 module.exports = (env, argv) => {
 
-    if (argv.mode === 'development') {
+    if (argv.mode === "development") {
         console.log("DEVELOPMENT")
-    } else if (argv.mode === 'production') {
+    } else if (argv.mode === "production") {
         console.log("PRODUCTION")
     } else {
         console.log("DEBUG")
