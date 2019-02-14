a4db21810c29e2856db1d47c1b0ca813d9b4d538
Kody King <kody.j.king@gmail.com>
2019-02-14 01:07:49 -0800

    Fixed computer not missing obvious winning moves.

--- src/engine/search.ts
+++ src/engine/search.ts
@@ -8,14 +8,26 @@ import { Color } from "./Color"
 
 const Pos = Position.create
 
-export default function(engine: Engine, options = { depth: 6, materialBias: 10 }) {
+export default function(engine: Engine, options = { depth: 6 }) {
     let startTime = Date.now()
     let evaluations = 0
     let cache = new Map<string, number>()
 
     const heuristic = (useFast) => {
+        const config = {
+            material: 10,
+            control: 5,
+            threat: 1,
+            threateningPieces: 1,
+            development: 5,
+            support: 5
+        }
+
+        if (engine.inCheck() && engine.inMate())
+            return engine.turn == Color.White ? -Infinity : Infinity
+
         if (useFast)
-            return engine.netMaterialValue * options.materialBias
+            return engine.netMaterialValue * config.material
 
         let control = 0
         let threat = 0
@@ -29,42 +41,50 @@ export default function(engine: Engine, options = { depth: 6, materialBias: 10 }
             return xDist + yDist
         }
 
-        for (let x = 0; x < 8; x++) {
-            for (let y = 0; y < 8; y++) {
-                let pos = Pos(x, y)
-                let piece = engine.pieces[pos]
-                let type = Piece.get.type(piece)
-                if (type === Type.Empty)
-                    continue
+        for (let pos = 0; pos < 64; pos++) {
+            let piece = engine.pieces[pos]
+            let type = Piece.get.type(piece)
+            if (type === Type.Empty)
+                continue
 
-                let invPieceValue = 1 / pieceValues[type]
-                let color = Piece.get.color(piece)
-                let valueSign = color === Color.White ? 1 : -1
+            let invPieceValue = 1 / pieceValues[type]
+            let color = Piece.get.color(piece)
+            let valueSign = color === Color.White ? 1 : -1
 
-                development += edgeDistance(pos) * invPieceValue * valueSign * 5
+            development += edgeDistance(pos) * invPieceValue * valueSign
+            if (type === Type.Pawn) {
+                let pawnDevelopment = (color == Color.White) ? 7 - Position.get.y(pos) : Position.get.y(pos)
+                development += Math.max(8, 1.5 ** pawnDevelopment) * valueSign
+            }
 
-                let threatening = false
-                for (let move of engine.generateMovesAt(pos, true)) {
-                    let capturedType = Piece.get.type(Move.get.captured(move))
-                    let capturedColor = Piece.get.color(Move.get.captured(move))
-                    let captured = capturedType !== Type.Empty
-                    if (captured && capturedColor == color) {
-                        support += valueSign * invPieceValue / pieceValues[capturedType]
-                    } else {
-                        let to = Move.get.to(move)
-                        control += (1 + edgeDistance(to) * invPieceValue) * valueSign
-                        if (captured) {
-                            threatening = true
-                            let value = capturedType == Type.King ? 10 : pieceValues[capturedType] * invPieceValue
-                            threat += valueSign * value
-                        }
+            let threatening = false
+            for (let move of engine.generateMovesAt(pos, true)) {
+                let capturedType = Piece.get.type(Move.get.captured(move))
+                let capturedColor = Piece.get.color(Move.get.captured(move))
+                let captured = capturedType !== Type.Empty
+                if (captured && capturedColor == color) {
+                    support += valueSign * invPieceValue / pieceValues[capturedType]
+                } else {
+                    let to = Move.get.to(move)
+                    control += (1 + edgeDistance(to) * invPieceValue) * valueSign
+                    if (captured) {
+                        threatening = true
+                        let value = capturedType == Type.King ? 10 : pieceValues[capturedType] * invPieceValue
+                        threat += valueSign * value
                     }
                 }
-                if (threatening)
-                    threateningPieces += valueSign
             }
+
+            if (threatening)
+                threateningPieces += valueSign
         }
-        return engine.netMaterialValue * options.materialBias + (control + threat + threateningPieces + development + support)
+
+        return engine.netMaterialValue * config.material
+            + control * config.control
+            + threat * config.threat
+            + threateningPieces * config.threateningPieces
+            + development * config.development
+            + support * config.support
     }
 
     const search = (depth = 0, rootCall = true, alpha = -Infinity, beta = Infinity) => {
@@ -78,7 +98,6 @@ export default function(engine: Engine, options = { depth: 6, materialBias: 10 }
         let positionString = engine.positionString() + depth + "," + turn
         if (!rootCall && cache.has(positionString))
             return cache.get(positionString) as number
-
 
         let pairs = engine.allMoves(true).map((move) => {
             engine.doMove(move)
@@ -94,18 +113,13 @@ export default function(engine: Engine, options = { depth: 6, materialBias: 10 }
         for (let [move, h] of pairs) {
             let value = 0
             engine.doMove(move)
-                // let capture = Piece.get.type(Move.get.captured(move)) != Type.Empty
-                // let _depth = (depth == 1 && capture) ? 1 : depth - 1
-                let _depth = depth - 1
-                value = (search(_depth, false, alpha, beta) as number)
+                value = (search(depth - 1, false, alpha, beta) as number)
                 // The advanced heuristic is too slow to use on leaves so instead it's evaluated on the parent.
                 if (depth == 1)
                     value += heuristic(false)
 
                 let isImprovement = best === null  || value * valueSign > bestValue * valueSign
                 if (isImprovement) {
-                    // console.log(engine.prettyString())
-                    // console.log(value)
                     best = move
                     bestValue = value
                     if (turn === Color.White)
@@ -121,10 +135,11 @@ export default function(engine: Engine, options = { depth: 6, materialBias: 10 }
 
         cache.set(positionString, bestValue)
 
-        return rootCall ? best : bestValue
+        return rootCall ? best: bestValue
     }
 
     let result = search(options.depth)
+
     let dt = (Date.now() - startTime)
 
     let addCommas = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
